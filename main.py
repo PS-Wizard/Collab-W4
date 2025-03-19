ec4ef71a544c5301084316a5ba33017e0bb6cd75
PS-Wizard <p.swoyam.1@gmail.com>
2025-03-19 12:25:58 +0545

    updated

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-def main():
-    userChoice = input("Enter an operation to perform ( + , - , * , / ) seperated by spaces: ")
-    num1,operator,num2 = userChoice.split(" ")
-    print(num1, operator, num2)
-
-
-
-
 def add(num1, num2):
     return num1 + num2
-
 def subtract(num1, num2):
     return num1 - num2
 
+def multiply(num1, num2):
+    return num1 * num2
 
-    
-def multiplication(num1, num2):
-    return(num1*num2)
-    
-main()
-def division(num1, num2):
-    if(num2 == 0):
-        print("Zero Division Error")
+def divide(num1, num2):
+    if num2 == 0:
         return "Division by 0 not allowed"
-    return (num1 / num2)
+    return num1 / num2
+
+def main():
+    user_choice = input("Enter an operation to perform ( + , - , * , / ) separated by spaces: ")
+    num1, operator, num2 = user_choice.split(" ")
+    num1, num2 = float(num1), float(num2)
+    
+    operations = {
+        '+': add,
+        '-': subtract,
+        '*': multiply,
+        '/': divide
+    }
+    
+    if operator in operations:
+        result = operations[operator](num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid operator")
 
 main()
