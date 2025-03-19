2617c4c766ac27295764b6aa59e3ba91953ff270
Bhawesh Ganesh <bhaweshganesh@Bhaweshs-MacBook-Pro.local>
2025-03-19 12:22:03 +0545

    multiplication function added

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     userChoice = input("Enter an operation to perform ( + , - , * , / ) seperated by spaces: ")
     num1,operator,num2 = userChoice.split(" ")
     print(num1, operator, num2)
-
+    
+def multiplication(num1, num2):
+    return(num1*num2)
+    
 main()
 
     
