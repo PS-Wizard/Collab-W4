719cdf17e3591ea27388214b8e3aebb67fd308d3
PS-Wizard <p.swoyam.1@gmail.com>
2025-03-19 11:53:19 +0545

    updated

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    userChoice = input("Enter an operation to perform ( + , - , * , / ) seperated by spaces: ")
+    num1,operator,num2 = userChoice.split(" ")
+    print(num1, operator, num2)
+
+main()
+
+    
+    
