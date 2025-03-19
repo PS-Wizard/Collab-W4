e276e6b971a021d06d9af100304c2f4b13fe72f0
Sandil Rai <sandilrai22@gmail.com>
2025-03-19 12:10:12 +0545

    Division function implement

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def main():
     num1,operator,num2 = userChoice.split(" ")
     print(num1, operator, num2)
 
-main()
+def division(num1, num2):
+    if(num2 == 0):
+        print("Zero Division Error")
+        return "Division by 0 not allowed"
+    return (num1 / num2)
 
-    
-    
+main()
